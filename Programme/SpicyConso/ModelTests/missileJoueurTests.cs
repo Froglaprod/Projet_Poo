b18562cdb79b63ed0ea46b1132d10acc16b38c13
Froglaprod <123393731+Froglaprod@users.noreply.github.com>
2023-11-01 18:59:46 +0100

    Refactorisation test unitaire

---
+++ Programme/SpicyConso/ModelTests/missileJoueurTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class missileJoueurTests
+    {
+        [TestMethod()]
+        public void UpdateMisilleTest()
+        {
+            // Arrange
+            missileJoueur missile = new missileJoueur(50);
+            int y = missile.y;
+            missile.missileIsLaunched = true;
+
+
+            // Act
+            missile.UpdateMisille();
+
+            // Assert
+            Assert.AreEqual(y - 1, missile.y);
+        }
+    }
+}
