0822b96a6e5f527e0fc3baa981ed94cd09cf0b80
Froglaprod <mathisbot@icloud.com>
2023-11-01 11:20:31 +0100

    Test unitaires

---
+++ Programme/SpicyConso/ModelTests/AlienTests.cs
@@ -0,0 +1,38 @@
+﻿using Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class AlienTests
+    {
+        [TestMethod()]
+        public void moveRightTest()
+        {
+            //Arrange
+
+
+            //Act
+
+            //Assert
+
+        }
+
+        [TestMethod()]
+        public void AlienTest()
+        {
+            // Arrange
+            int x = 1;
+            int y = 2;
+            int hpDefault = 100;
+
+            // Act
+            Alien alien = new Alien(x, y, hpDefault);
+
+            // Assert
+            Assert.AreEqual(x, alien.x);
+            Assert.AreEqual(y, alien.y);
+            Assert.AreEqual(hpDefault, alien.hpDefault);
+        }
+    }
+}
